19e1884130d8bc8a60ec6e95badad9c0c1e7cdf7
karanpal-dhillon <karanpal.dhillon@gmail.com>
2012-05-24 23:02:58 +0530

    added test main class.To check the Github integration

---
+++ TestApp/src/book/prospring/chapter01/TestMainApp.java
@@ -0,0 +1,13 @@
+package book.prospring.chapter01;
+
+public class TestMainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is a test app to learn spring framework using pro spring 3 book.");
+
+	}
+
+}
